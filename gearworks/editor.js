62793116994bf21c136631268272dd5d6c6a0018
hiruevan <98787662+hiruevan@users.noreply.github.com>
2023-09-27 21:53:16 -0600

    Update editor.js

--- gearworks/editor.js
+++ gearworks/editor.js
@@ -17,12 +17,12 @@ function toggleSideBar() {
     }
 }
 
-function convertHtmlToStr(html) {
+function convertHtmlToStr(html, br = "\n") {
     let ret = "";
     for (let i = 0; i < html.length; i++) {
         if (html[i] === "<") {
             if (html[i+1] === "b" && html[i+2] === "r") {
-                ret += "\n";
+                ret += br;
             }
             while (!(html[i] === ">")) {
                 i++;
@@ -35,6 +35,39 @@ function convertHtmlToStr(html) {
     return ret;
 }
 
+function colorWord(text, word, classifier) {
+    if (text.includes(word)) {
+        text = text.replaceAll(word, "<span class='" + classifier + "'>" + word + "</span>");
+    }
+    return text;
+}
+
+function colorComments(text) {
+    let ret = text + " ";
+    if (text.includes("//")) {
+        let commentOpen = false;
+        ret = "";
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] === "/" && text[i+1] === "/") {
+                commentOpen = true;
+                ret += "<span class='comment'>";
+            }
+            if (text[i] === "<" && commentOpen) {
+                if (text[i+1] === "b" && text[i+2] === "r") {
+                    ret += "</span>";
+                } else {
+                    while (!(text[i-1] === ">")) {
+                        i++;
+                    }
+                }
+            }
+            ret += text[i];
+        }
+        
+    }
+    return ret;
+}
+
 
 // Text Editor Zoom code
 // Coming soon (CSS is done)
@@ -43,17 +76,28 @@ function convertHtmlToStr(html) {
 // Lines in editor (Credit to stack overflow post https://stackoverflow.com/questions/60106046/how-to-display-row-number-next-to-each-row-on-contenteditable-div)
 $(document).ready(function(){
     $('#edit').css('min-height', $('#edit').height());
-    $('#edit').html('// Welcome to the Gear Works Editor!');
+    $('#edit').html('');
     var currentHeight = $('#edit').height();
     var lineHeight = currentHeight;
-    $('#edit').keyup(function(){
-      if($(this).height()!=currentHeight){
-        currentHeight = $(this).height();
-        var lines = currentHeight/lineHeight;
-        $('#nums').html('')
-        for (i = 1; i < lines+1; i++) {
-          $('#nums').append('<span>'+i+'</span>')
+    $('#edit').on("keydown keyup", function(){
+        setColors("edit");
+        if($(this).height()!=currentHeight){
+                currentHeight = $(this).height();
+                var lines = currentHeight/lineHeight;
+                $('#nums').html('');
+            for (i = 1; i < lines+1; i++) {
+                $('#nums').append('<span>'+i+'</span>');
+            }
         }
-      }
-    });
-  });
+    })
+})
+
+function setColors() {
+    let raw = document.getElementById("edit").innerHTML;
+    raw = colorWord(raw, "=", "operator");
+    raw = colorWord(raw, "function", "fun");
+    raw = colorWord(raw, "+", "operator");
+    raw = colorWord(raw, "-", "operator");
+    //raw = colorComments(raw); (glitchy)
+    document.getElementById("display").innerHTML = raw;
+}
