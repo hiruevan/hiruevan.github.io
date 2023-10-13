634cdf8d26a1c3e4bc4a490c785eff881c02f0e1
parkmasterpro <92823117+Parkmasterpro@users.noreply.github.com>
2023-10-13 10:15:56 -0600

    Update gwLib.js
    added objects

--- gearworks/gwLib.js
+++ gearworks/gwLib.js
@@ -1,2 +1,15 @@
 const gw_canvas = document.getElementById('cvs');
 const draw = gw_canvas.getContext("2d");
+const gw =
+{
+ log: function(str) {
+   console.log(str);
+   //for the gearworks log
+  } 
+}
+
+const draw =
+{
+  thisIsAFunction: function() {
+  }
+}
