dac629b3096111eb6db0c90cd11d8ad3f8cece08
hiruevan <98787662+hiruevan@users.noreply.github.com>
2024-01-13 15:32:31 -0700

    Update gwLib.js

--- gearworks/gwLib.js
+++ gearworks/gwLib.js
@@ -14,16 +14,14 @@ gw_ctx.background = function(color) {
 const draw = gw_ctx;
 
 // gearWorks obj
-const gearWorks = {};
-gearWorks.log = function(log) {
-    console.log(log);
-    gw_log(log);
-}
-gearWorks.warn = function(log) {
-    console.warn(warn);
-    gw_warn(log);
-}
-gearWorks.error = function(log) {
-    console.error(log);
-    gw_error(log);
-}
+const gearWorks = {
+    log: function(log) {
+        gw_log(log)
+    },
+    warn: function(log) {
+        gw_warn(log);
+    },
+    error: function(log) {
+        gw_error(log)
+    }
+};
