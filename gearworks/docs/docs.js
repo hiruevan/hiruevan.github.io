36823005c75b73acab435a2d45117f4f1e146687
Cap'n Sushi <mkimmel17@gmail.com>
2024-01-31 16:01:18 -0700

    fix scary docs code actually really close though there were just a few syntax errors and i cleaned the code up a touch

--- gearworks/docs/docs.js
+++ gearworks/docs/docs.js
@@ -1,45 +1,27 @@
-const varNames = [
-'x',
-'y',
-'z',
-'w',
-'a',
-'b',
-'c',
-];
-const varType = [
-'str',
-'int',
-'float',
-'bool',
-];
+const varNames = ['x', 'y', 'z', 'w', 'a', 'b', 'c'];
+const varType = ['str', 'int', 'float', 'bool'];
 
-
-    for(let i in varNames)
-    {
-        document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(i + ",", '<span class="var">' + i +'</span> '+ ',');
-        document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(i + ")", '<span class="var">' + i +'</span>' + ')');
-    }
-    for(let i in varType)
-    {
-        document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(i, '<span class="vartype">' + i + '</span>');
-        document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(i, '<span class="vartype">' + i + '</span>');
-    }
-
-
-function createTag(type, inner)
-{
-   return document.createElement(type).appendChild(document.createTextNode(inner));
+for (let i in varNames) {
+    document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varNames[i] + ",", '<span class="var">' + varNames[i] + '</span> ' + ',');
+    document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varNames[i] + ")", '<span class="var">' + varNames[i] + '</span>' + ')');
 }
 
+for (let i in varType) {
+    document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varType[i], '<span class="vartype">' + varType[i] + '</span>');
+    document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varType[i], '<span class="vartype">' + varType[i] + '</span>');
+}
+
+function createTag(type, inner) {
+    const tag = document.createElement(type);
+    tag.appendChild(document.createTextNode(inner));
+    return tag;
+}
 
 document.head.appendChild(createTag('style', `
 .var{
-color:skyblue;
-{
+    color: skyblue;
+}
 .vartype{
-color:orange;
+    color: orange;
 }
 `));
-
-
