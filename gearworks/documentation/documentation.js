148390cee1d01f03601ae154e1c29e6a81c343fb
parkmasterpro <92823117+Parkmasterpro@users.noreply.github.com>
2024-01-30 19:30:25 -0700

    Create documentation.js

---
+++ gearworks/documentation/documentation.js
@@ -0,0 +1,40 @@
+const varNames = [
+'x',
+'y',
+'z',
+'w',
+'a',
+'b',
+'c',
+];
+const varType = [
+'str',
+'int',
+'float',
+'bool',
+];
+const b = document.body;
+
+
+for(v in varNames)
+{
+    b.innerHtml.replaceAll(v + ",", '<span class="var">'+v+'</span>'+',');
+    b.innerHtml.replaceAll(v + ")", '<span class="var">'+v+'</span>'+')');
+}
+for(t in vartype)
+{
+    b.innerHtml.replaceAll(v, '<span class="vartype">'+t+'</span>');
+    b.innerHtml.replaceAll(v, '<span class="vartype">'+t+'</span>');
+}
+
+
+document.head.innerHTML = document.head.innerHTML + `
+<style>
+.var{
+color:skyblue;
+}
+.vartype
+{
+color:orange;
+}
+</style>`
