ff80dbfaba1e235307e536c048847f6ac9f65fdd
parkmasterpro <92823117+Parkmasterpro@users.noreply.github.com>
2024-02-02 18:46:01 -0700

    canged order to fix the styling

--- gearworks/docs/docs.js
+++ gearworks/docs/docs.js
@@ -2,20 +2,24 @@ const varNames = ['x', 'y', 'z', 'w', 'a', 'b', 'c'];
 const varType = ['str', 'int', 'float', 'bool', 'color', 'array', 'obj', 'class', 'func'];
 const objs = ['gearWorks', 'draw', 'Math', 'document', 'console'];
 
-for (let i in varNames) {
-    document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varNames[i] + ",", '<span class="var">' + varNames[i] + '</span> ' + ',');
-    document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varNames[i] + ")", '<span class="var">' + varNames[i] + '</span>' + ')');
-}
+
 
 for (let i in varType) {
     document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varType[i], '<span class="vartype">' + varType[i] + '</span>');
     document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varType[i], '<span class="vartype">' + varType[i] + '</span>');
 }
-
 for (let i in objs) {
     document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(objs[i], '<span class="objs">' + objs[i] + '</span>');
     document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(objs[i], '<span class="objs">' + objs[i] + '</span>');
 }
+for (let i in varNames) {
+    document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varNames[i] + ",", '<span class="var">' + varNames[i] + '</span> ' + ',');
+    document.getElementsByClassName('container')[0].innerHTML = document.getElementsByClassName('container')[0].innerHTML.replaceAll(varNames[i] + ")", '<span class="var">' + varNames[i] + '</span>' + ')');
+}
+
+
+
+
 
 function createTag(type, inner) {
     let tag = document.createElement(type);
